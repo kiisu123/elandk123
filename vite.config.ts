c66f06100ce2a51947f0acfbe40b8c7c21c9c9b3
kiisu123 <takashimakiisu@gmail.com>
2025-07-02 22:59:23 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/elandk123/',  
+  base: '/',  
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
